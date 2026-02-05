d0235eebd1af94192095856918d2aa37ac79d9fc
@gjaswskfd <a0976998776@gmail.com>
2026-02-05 10:08:00 +0800

    climber-fix

--- src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
@@ -6,10 +6,5 @@ public class ClimberConstants {
 
     public static final double climberKP = 0.1;
     public static final double climberKI = 0.0; 
-    public static final double climberKD = 0.0;
-
-
-
-
-    
+    public static final double climberKD = 0.0; 
 }
