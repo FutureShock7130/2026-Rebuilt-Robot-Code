f40656f78882ed25f4fdf0d19769c117fda2c4e3
oscarlustsrrail <oscarlu.starrail@gmail.com>
2026-02-06 10:44:08 +0800

    create shooter and indexer

---
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -0,0 +1,60 @@
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
+
+public class Shoot extends Command {
+    private final ShooterSubsystem shooterSubsystem;
+    private final Supplier<Pose2d> robotPoseSupplier;
+    private final InterpolatingDoubleTreeMap speedUpMap = new InterpolatingDoubleTreeMap();
+    private final InterpolatingDoubleTreeMap speedDownMap = new InterpolatingDoubleTreeMap();
+    private final InterpolatingDoubleTreeMap angleMap = new InterpolatingDoubleTreeMap();
+    public Shoot(
+        ShooterSubsystem shooterSubsystem,
+        Supplier<Pose2d> robotPoseSupplier
+    ) {
+        this.shooterSubsystem = shooterSubsystem;
+        this.robotPoseSupplier = robotPoseSupplier;
+        addRequirements(shooterSubsystem);
+
+        speedUpMap.put(0.0, 0.0);//rpm
+        speedUpMap.put(2.0, 1.0);//rpm
+        speedDownMap.put(0.0, 0.0);//rpm
+        speedDownMap.put(0.0, 0.0);//rpm
+        angleMap.put(0.0, 0.0);
+        angleMap.put(0.0, 0.0);
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        Pose2d currentPose = robotPoseSupplier.get();
+        Translation2d goalPose = new Translation2d(2, 2);
+        double distance = currentPose.getTranslation().getDistance(goalPose);
+        // some calculations here
+        double shooterUpSpeed = speedUpMap.get(distance); // replace with actual calculation
+        shooterSubsystem.setShooterUpSpeed(shooterUpSpeed);
+        double ShooterDownSpeed = speedDownMap.get(distance);
+        shooterSubsystem.setShooterDownSpeed(ShooterDownSpeed);
+        double shooterAngleTarget = angleMap.get(distance);
+        shooterSubsystem.setShooterTarget(shooterAngleTarget);       
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooterSubsystem.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -0,0 +1,7 @@
+package  frc.robot.subsystems.indexer;
+public class IndexerConstants{
+    public static final int indexerUpID = 53;
+    public static final int indexerDownID = 54;
+    public static final double indexerUpSpeed = 0.0;
+    public static final double indexerDownSpeed = 0.0;
+}
