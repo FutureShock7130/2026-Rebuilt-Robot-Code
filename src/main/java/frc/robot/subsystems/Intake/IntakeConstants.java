0c867a016897a68a7419ae02b3b96a6507869c00
@gjaswskfd <a0976998776@gmail.com>
2026-02-24 20:30:54 +0800

    0224

---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.Intake;
+
+import com.ctre.phoenix6.CANBus;
+
+public class IntakeConstants {
+    public static final CANBus intakeBus = new CANBus("GTX7130");
+    public static final int intakeMotorID = 30;
+}
---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.Intake;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+
+public class IntakeSubsystem {
+    
+    TalonFX intakeMotor = new TalonFX(IntakeConstants.intakeMotorID,IntakeConstants.intakeBus);
+    public IntakeSubsystem() {
+        TalonFXConfiguration intakeConfiguration = new TalonFXConfiguration()
+        .withFeedback(new FeedbackConfigs()
+        .withSensorToMechanismRatio(12))
+        .withMotorOutput(new MotorOutputConfigs());        
+    }
+    
+    public void runIntake(){
+        intakeMotor.set(1);
+    }
+    public void stopIntake(){
+        intakeMotor.set(0);
+    }
+}
